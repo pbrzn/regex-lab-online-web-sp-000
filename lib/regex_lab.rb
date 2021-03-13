fa85bd5a71eddcef0c2664ab2af056591b632c8b
jolly-technologist-1233 <pbrennanmusic@gmail.com>
2021-03-13 21:45:19 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -15,5 +15,5 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-  !!phone.scan(/(\b\d{3}\d{3}\d{4}\b)|(\b\d{3}.\d{3}.\d{4}\b)/)
+  !!phone.scan(/.(\d{3})(\d{3})(\d{4}).|.(\d{3}).(\d{3}).(\d{4})./)
 end
