c30c6315da772f1a31bb86ef2400919fc5c04e93
jolly-technologist-1233 <pbrennanmusic@gmail.com>
2021-03-13 21:10:25 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,5 +1,5 @@
 def starts_with_a_vowel?(word)
-  !!word.scan(/\b[aeiou].+\b/)
+  !!word.scan(/\b[aeiou]\W+\b/)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
@@ -15,5 +15,5 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-  !!phone.scan(/(\d{3})-(\d{3})-(\d{4})/)
+  !!phone.scan(/(\d{3}).(\d{3}).(\d{4})/)
 end
